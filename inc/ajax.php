b124068f9958ce503ccdbcaa39a3315dde60b810
Peter <Peter@Peter-PC>
2013-11-26 19:40:18 +0000

    added function to return share link order

--- inc/ajax.php
+++ inc/ajax.php
@@ -63,3 +63,52 @@ function kebo_se_save_dashboard_config() {
     
 }
 add_action( 'wp_ajax_kebo_se_save_dashboard_config', 'kebo_se_save_dashboard_config' );
+
+/*
+ * AJAX Save Share Link Order
+ */
+function kbso_save_sharelink_order() {
+    
+    // Get the action
+    $action = $_POST['action'];
+    
+    // Get the data
+    $data = $_POST['data'];
+    
+    // Get nonce
+    $nonce = $_POST['nonce'];
+    
+    /*
+     * Check action
+     */
+    if ( 'kbso_save_sharelink_order' !== $action ) {
+        die();
+    }
+    
+    /*
+     * Check nonce
+     */
+    if ( ! wp_verify_nonce( $nonce, 'kbso_sharelink_order' ) ) {
+        die();
+    }
+    
+    // Save Dashboard Positions
+    update_option( 'kbso_sharelink_order', $data );
+    
+    // Send successful response
+    $response = array(
+        'action' => 'save',
+        'success' => 'true',
+    );
+    
+    // Clear and previous output, like errors.
+    ob_clean();
+    
+    // Output response
+    print_r( json_encode( $response ) );
+    
+    // Send Output
+    die();
+    
+}
+add_action( 'wp_ajax_kbso_save_sharelink_order', 'kbso_save_sharelink_order' );
